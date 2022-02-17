5703911e5764896dcab7c0baa7270fcc3d220696
ENICharles <99649957+ENICharles@users.noreply.github.com>
2022-02-17 18:30:53 +0100

    Delete DAOUser.java

--- src/main/java/fr/eni/enchere/dao/DAOUser.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.enchere.dao;
-
-import fr.eni.enchere.bo.Utilisateur;
-
-public interface DAOUser
-{
-	Utilisateur getUserConnection(String id,String paswd) throws DAOException;
-	void createUser(Utilisateur user) throws DAOException;
-}
